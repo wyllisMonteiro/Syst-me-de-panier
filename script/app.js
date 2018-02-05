a648884a90278f9c3428ea85e8d583b189608c4b
wyllis <wyllismonteiro@gmail.com>
2018-02-06 00:51:55 +0100

    Utilisation des keyCode

--- script/app.js
+++ script/app.js
@@ -17,10 +17,34 @@ let produits = [{
     "lien" : "https://picsum.photos/200/100?image="    
 }];
 
-let produitsHtml = $('.produits');
-    for(let compteur = 0; compteur < produits.length ;compteur++) {
-        let result = produits[compteur];
 
-        produitsHtml.append('<a href=""><div class="item"><h1>' + result.nom + '</h1>' + '<h2>' + result.type + '</h2><img src="' + result.lien + result.compteur + '"></div></a>');
+// Fonctions
+function switchItem(evt) {
+    if(evt.keyCode === 39) {
+        console.log('yes');
     }
+    if(evt.keyCode === 37) {
+        console.log('non');
+    }
+    if(evt.keyCode === 13) {
+        console.log('lol');
+    }
+}
+
+
+//Main
+let produitsHtml = $('.produits');
+
+for(let compteur = 0; compteur < produits.length ;compteur++) {
+    let result = produits[compteur];
+
+    produitsHtml.append('<a href=""><div class="item"><h1>' + result.nom + '</h1>' + '<h2>' + result.type + '</h2><img src="' + result.lien + result.compteur + '"></div></a>');
+}
+
+let divItems = $('.item');
+divItems[0].classList.add('selected');
+
+$(document).on('keyup',switchItem)
+
+
     
