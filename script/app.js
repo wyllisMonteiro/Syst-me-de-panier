e78852d4008efcfb3ee2c732e5281ac025cb650c
wyllis <wyllismonteiro@gmail.com>
2018-02-06 00:28:56 +0100

    CSS correct des items

--- script/app.js
+++ script/app.js
@@ -1,19 +1,25 @@
 let produits = [{
     "nom" : "Fraise",
-    "type" : "fruit",
+    "type" : "Fruit",
     "compteur" : 1,
-    "lien" : "https://picsum.photos/600/300?image="
+    "lien" : "https://picsum.photos/200/100?image="
 },
 {
     "nom" : "Pomme",
-    "type" : "fruit",
+    "type" : "Fruit",
     "compteur" : 2,    
-    "lien" : "https://picsum.photos/600/300?image="    
+    "lien" : "https://picsum.photos/200/100?image="    
+},
+{
+    "nom" : "Pâtes",
+    "type" : "Protéines",
+    "compteur" : 3,    
+    "lien" : "https://picsum.photos/200/100?image="    
 }];
 
 let produitsHtml = $('.produits');
     for(let compteur = 0; compteur < produits.length ;compteur++) {
         let result = produits[compteur];
-        produitsHtml.append('<div cless="panier"><p>' + result.nom + '</p>' + '<p>' + result.type + '</p><img src="' + result.lien + result.compteur + '"></div>');
+        produitsHtml.append('<a href=""><div class="item"><h1>' + result.nom + '</h1>' + '<h2>' + result.type + '</h2><img src="' + result.lien + result.compteur + '"></div></a>');
     }
     
