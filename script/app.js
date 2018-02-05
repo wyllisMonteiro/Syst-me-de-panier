bd8225a905b64cce880a3d6e586d99422612c682
Wyllis <wyllismonteiro@gmail.com>
2018-02-05 19:57:42 +0100

    Affichage des produits

--- script/app.js
+++ script/app.js
@@ -1,14 +1,19 @@
 let produits = [{
     "nom" : "Fraise",
-    "type" : "fruit"
+    "type" : "fruit",
+    "compteur" : 1,
+    "lien" : "https://picsum.photos/600/300?image="
 },
 {
     "nom" : "Pomme",
-    "type" : "fruit"
+    "type" : "fruit",
+    "compteur" : 2,    
+    "lien" : "https://picsum.photos/600/300?image="    
 }];
 
 let produitsHtml = $('.produits');
     for(let compteur = 0; compteur < produits.length ;compteur++) {
-        produitsHtml.append('<li>' + produits[compteur].nom + '</li>');
+        let result = produits[compteur];
+        produitsHtml.append('<div cless="panier"><p>' + result.nom + '</p>' + '<p>' + result.type + '</p></div><img src="' + result.lien + result.compteur + '">');
     }
     
