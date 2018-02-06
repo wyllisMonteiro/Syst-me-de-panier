3b20578437ea331a64c8204def95a3b04e5aa260
wyllis <wyllismonteiro@gmail.com>
2018-02-06 17:02:34 +0100

    JS des flèches gauche et droite

--- script/app.js
+++ script/app.js
@@ -21,10 +21,28 @@ let produits = [{
 // Fonctions
 function switchItem(evt) {
     if(evt.keyCode === 39) {
-        console.log('yes');
+        let selected = $('.selected');
+        let toSelect = selected.parent().next().find('div');
+        
+        if(toSelect.length === 0) {
+            selected.removeClass('selected');
+            $('a:first-child').find('.item').addClass('selected');
+        } else {
+            selected.removeClass('selected');
+            toSelect.addClass('selected');
+        }
     }
     if(evt.keyCode === 37) {
-        console.log('non');
+        let selected = $('.selected');
+        let toSelect = selected.parent().prev().find('div');
+        
+        if(toSelect.length === 0) {
+            selected.removeClass('selected');
+            $('a:last-child').find('.item').addClass('selected');
+        } else {
+            selected.removeClass('selected');
+            toSelect.addClass('selected');
+        }
     }
     if(evt.keyCode === 13) {
         console.log('lol');
@@ -38,7 +56,7 @@ let produitsHtml = $('.produits');
 for(let compteur = 0; compteur < produits.length ;compteur++) {
     let result = produits[compteur];
 
-    produitsHtml.append('<a href=""><div class="item"><h1>' + result.nom + '</h1>' + '<h2>' + result.type + '</h2><img src="' + result.lien + result.compteur + '"></div></a>');
+    produitsHtml.append('<a href="#"><div class="item"><h1>' + result.nom + '</h1>' + '<h2>' + result.type + '</h2><img src="' + result.lien + result.compteur + '"></div></a>');
 }
 
 let divItems = $('.item');
